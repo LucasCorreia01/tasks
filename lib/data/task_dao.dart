033bd08d3e0dad2fa5cfba9fe6b1c13fe55e0244
LucasCorreia01 <lucascorreiadamaceno@gmail.com>
2023-07-03 09:18:10 -0300

    implementação do CRUD

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -6,63 +6,61 @@ class TaskDao {
   static String tableSql = 'CREATE TABLE $_tableName('
       '$_name TEXT,'
       '$_difficulty INTEGER,'
-      '$_image TEXT,'
+      '$_image TEXT'
       ')';
 
   static const String _tableName = 'TaskTable';
   static const String _name = 'name';
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
-  static const String _level = 'level';
 
   //Salva uma nova tarefa em nosso banco de dados;
   save(Task tarefa) async {
-    // print('Estamos acessando o método save: ');
+    print('Estamos acessando o método save: ');
     final Database database = await getDatabase();
     var itemExists = await find(tarefa.nameTask);
     Map<String, dynamic> taskMap = toMap(tarefa);
     if (itemExists.isEmpty) {
-      // print('A tarefa não existia');
+      print('A tarefa não existia');
       return await database.insert(_tableName, taskMap);
     } else {
-      // print('A tarefa já exisite');
+      print('A tarefa já exisite');
       return await database.update(_tableName, taskMap,
           where: '$_name = ?', whereArgs: [tarefa.nameTask]);
     }
   }
 
-  //método que recupera todos as tarefas;
+  //método que recupera todos as tarefas
   Future<List<Task>> findAll() async {
-    // print('Estamos acessando o findAll: ');
+    print('Estamos acessando o findAll: ');
     final Database database = await getDatabase();
     final List<Map<String, dynamic>> result = await database.query(_tableName);
-    // print('Procurando dados no banco de dados... encontrado: $result');
+    print('Procurando dados no banco de dados... encontrado: $result');
 
     return toList(result);
   }
 
-  //método que encontra uma tarefa única com base no seu nome;
+  //método que encontra uma tarefa única com base no seu nome
   Future<List<Task>> find(String nameOfTask) async {
-    // print('Estamos acessando o find: ');
+    print('Estamos acessando o find: ');
     final Database database = await getDatabase();
     final List<Map<String, dynamic>> result = await database.query(_tableName,
         columns: [_name], where: '$_name LIKE ?', whereArgs: [nameOfTask]);
 
-    // print('Tarefa encontrada: ${toList(result)}');
+    print('Tarefa encontrada: ${toList(result)}');
     return toList(result);
   }
 
-  //apaga uma tarefa baseado no nome armazenado.
   delete(String nameOfTask) async {
-    // print('Deletando tarefa: $nameOfTask');
+    print('Estamos acessando o método delete');
     final Database database = await getDatabase();
-    return database
-        .delete(_tableName, where: '$_name = ?', whereArgs: [nameOfTask]);
+    return await database.delete(_tableName, where: '$_name = ?', whereArgs: [nameOfTask]);
+
   }
 
-  //Transforma nosso mapa vindo do banco em um lista de tarefas para a nossa aplicação;
+  //Transforma nosso mapa vindo do banco em um lista de tarefas para a nossa aplicação.
   List<Task> toList(List<Map<String, dynamic>> mapOfTasks) {
-    // print('Convertendo to List:');
+    print('Convertendo to List:');
     final List<Task> tarefas = [];
     for (Map<String, dynamic> linha in mapOfTasks) {
       final Task tarefa = Task(
@@ -72,18 +70,18 @@ class TaskDao {
       tarefas.add(tarefa);
     }
 
-    // print(' Lista de tarefas: $tarefas');
+    print(' Lista de tarefas: $tarefas');
     return tarefas;
   }
 
-  //Transforma nossa tarefa em um mapa para salvarmos em nosso banco de dados;
+  //Transforma nossa tarefa em um mapa para savarmos
   Map<String, dynamic> toMap(Task tarefa) {
-    // print('Convertendo tarefa em Map: ');
+    print('Convertendo tarefa em Map: ');
     final Map<String, dynamic> mapOfTasks = Map();
     mapOfTasks[_name] = tarefa.nameTask;
     mapOfTasks[_difficulty] = tarefa.difficulty;
     mapOfTasks[_image] = tarefa.urlImage;
-    // print('Mapa de tarefas: $mapOfTasks');
+    print('Mapa de tarefas: $mapOfTasks');
 
     return mapOfTasks;
   }
